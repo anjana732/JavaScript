4a7ea7e46ac305f4fc4ad6b82fac542bf7afef3a
anjana732 <anjanasingh1257@gmail.com>
2024-01-19 16:47:21 +0530

    Learnt about reference dataType

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-console.log("Anjana")
+// console.log("Anjana")
 
 //var, let and const 
 
@@ -7,6 +7,32 @@ var Name = "Anjana"
 let email = "anjana@gmail.com"
 city = "Hajipur"
 
-console.table([id, Name, email, city])
+// console.table([id, Name, email, city])
 
 //Data Types
+// console.log(typeof id)
+// console.log(typeof Name)
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+// Premitive dataType
+
+let name1 = "anjana"
+let name2 = name1
+name2="anjanasingh"
+console.log(name1)
+console.log(name2)
+
+//reference dataType
+
+let userOne = {
+    name:"Anjana",
+    email: "anjana@gmail.com"
+}
+
+let userTwo = userOne
+
+userTwo.email = "anjanasingh@gmail.com"
+
+console.log(userOne)
+
