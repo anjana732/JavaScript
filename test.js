d305b3dd1887273f2f6cdfb729e61e97638ee4b5
anjana732 <anjanasingh1257@gmail.com>
2024-01-20 20:32:38 +0530

    Date

--- test.js
+++ test.js
@@ -68,17 +68,36 @@ const newNumber = 1234.567
 // console.log(newNumber.toPrecision(5))
 // console.log(newNumber.toLocaleString('en-IN'))
 
-console.log(Math.abs(-8))
-console.log(Math.round(8.7))
-console.log(Math.round(8.4))
-console.log(Math.ceil(4.2))
-console.log(Math.floor(4.9))
+// console.log(Math.abs(-8))
+// console.log(Math.round(8.7))
+// console.log(Math.round(8.4))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
 
-console.log(Math.random())
-console.log((Math.random()*10) + 1)
-console.log(Math.floor((Math.random()*10) + 1))
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor((Math.random()*10) + 1))
 
 const min = 10
 const max = 20
 
-console.log(Math.floor((Math.random()*(max - min + 1 ))+min))
+// console.log(Math.floor((Math.random()*(max - min + 1 ))+min))
+
+//Dates in JS
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+let myCreatedDate = new Date(2023, 0, 7, 5,30)
+console.log(myCreatedDate.toLocaleString())
+
+let newDate = new Date("2023-01-12")
+console.log(newDate.toString())
+
+//console.log(Date.now())
+let myNewDate = Date.now()
+console.log(myNewDate.getTim)
