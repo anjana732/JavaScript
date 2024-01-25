e14920b30f5902272372e864388240a6ed763679
anjana732 <anjanasingh1257@gmail.com>
2024-01-25 16:44:23 +0530

    chaining of function

--- test.js
+++ test.js
@@ -407,7 +407,7 @@ const myNewArray2 = [1,2,3,4,5,6,7,8,9,10]
 
  //console.log(mapResult)
 
- //chaining of function 
+ //chaining og map and filter function 
 
  const chaining = arr2.map((nums) => nums * 10).map((nums) => nums + 1).filter((nums) => nums >= 40)
 
