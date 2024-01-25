1b13c92923d3899d06e5b80bd89160e285c57339
anjana732 <anjanasingh1257@gmail.com>
2024-01-25 16:23:23 +0530

    Json Read

---
+++ JsonRead.js
@@ -0,0 +1,378 @@
+
+  const books = [
+      {
+        "title": "The Catcher in the Rye",
+        "author": "J.D. Salinger",
+        "publishedYear": 1951,
+        "genre": "Fiction",
+        "isbn": "978-0-316-76948-0",
+        "publisher": "Little, Brown and Company",
+        "language": "English",
+        "pages": 277
+      },
+      {
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "publishedYear": 1960,
+        "genre": "Fiction",
+        "isbn": "978-0-06-112008-4",
+        "publisher": "J.B. Lippincott & Co.",
+        "language": "English",
+        "pages": 281
+      },
+      {
+        "title": "1984",
+        "author": "George Orwell",
+        "publishedYear": 1949,
+        "genre": "Dystopian Fiction",
+        "isbn": "978-0-452-28423-4",
+        "publisher": "Secker & Warburg",
+        "language": "English",
+        "pages": 328
+      },
+      {
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "publishedYear": 1925,
+        "genre": "Fiction",
+        "isbn": "978-0-7432-7356-5",
+        "publisher": "Scribner",
+        "language": "English",
+        "pages": 180
+      },
+      {
+        "title": "Pride and Prejudice",
+        "author": "Jane Austen",
+        "publishedYear": 1813,
+        "genre": "Romance",
+        "isbn": "978-0-19-283355-4",
+        "publisher": "T. Egerton",
+        "language": "English",
+        "pages": 279
+      },
+      {
+        "title": "One Hundred Years of Solitude",
+        "author": "Gabriel García Márquez",
+        "publishedYear": 1967,
+        "genre": "Magical Realism",
+        "isbn": "978-0-06-112008-4",
+        "publisher": "Harper & Row",
+        "language": "Spanish",
+        "pages": 417
+      },
+      {
+        "title": "The Hobbit",
+        "author": "J.R.R. Tolkien",
+        "publishedYear": 1937,
+        "genre": "Fantasy",
+        "isbn": "978-0-618-51756-7",
+        "publisher": "Allen & Unwin",
+        "language": "English",
+        "pages": 310
+      },
+      {
+        "title": "The Lord of the Rings",
+        "author": "J.R.R. Tolkien",
+        "publishedYear": 1954,
+        "genre": "Fantasy",
+        "isbn": "978-0-345-24049-8",
+        "publisher": "Allen & Unwin",
+        "language": "English",
+        "pages": 1178
+      },
+      {
+        "title": "Harry Potter and the Philosopher's Stone",
+        "author": "J.K. Rowling",
+        "publishedYear": 1997,
+        "genre": "Fantasy",
+        "isbn": "978-0-7475-3269-6",
+        "publisher": "Bloomsbury",
+        "language": "English",
+        "pages": 223
+      },
+      {
+        "title": "Harry Potter and the Chamber of Secrets",
+        "author": "J.K. Rowling",
+        "publishedYear": 1998,
+        "genre": "Fantasy",
+        "isbn": "978-0-7475-3849-0",
+        "publisher": "Bloomsbury",
+        "language": "English",
+        "pages": 251
+      },
+      {
+        "title": "Harry Potter and the Prisoner of Azkaban",
+        "author": "J.K. Rowling",
+        "publishedYear": 1999,
+        "genre": "Fantasy",
+        "isbn": "978-0-7475-4215-5",
+        "publisher": "Bloomsbury",
+        "language": "English",
+        "pages": 317
+      },
+      {
+        "title": "Harry Potter and the Goblet of Fire",
+        "author": "J.K. Rowling",
+        "publishedYear": 2000,
+        "genre": "Fantasy",
+        "isbn": "978-0-7475-4624-5",
+        "publisher": "Bloomsbury",
+        "language": "English",
+        "pages": 636
+      },
+      {
+        "title": "Harry Potter and the Order of the Phoenix",
+        "author": "J.K. Rowling",
+        "publishedYear": 2003,
+        "genre": "Fantasy",
+        "isbn": "978-0-7475-5100-3",
+        "publisher": "Bloomsbury",
+        "language": "English",
+        "pages": 766
+      },
+      {
+        "title": "Harry Potter and the Half-Blood Prince",
+        "author": "J.K. Rowling",
+        "publishedYear": 2005,
+        "genre": "Fantasy",
+        "isbn": "978-0-7475-8108-8",
+        "publisher": "Bloomsbury",
+        "language": "English",
+        "pages": 607
+      },
+      {
+        "title": "Harry Potter and the Deathly Hallows",
+        "author": "J.K. Rowling",
+        "publishedYear": 2007,
+        "genre": "Fantasy",
+        "isbn": "978-0-7475-9485-5",
+        "publisher": "Bloomsbury",
+        "language": "English",
+        "pages": 607
+      },
+      {
+        "title": "The Alchemist",
+        "author": "Paulo Coelho",
+        "publishedYear": 1988,
+        "genre": "Fiction",
+        "isbn": "978-0-06-112008-4",
+        "publisher": "HarperCollins",
+        "language": "Portuguese",
+        "pages": 177
+      },
+      {
+        "title": "The Da Vinci Code",
+        "author": "Dan Brown",
+        "publishedYear": 2003,
+        "genre": "Mystery",
+        "isbn": "978-0-385-50420-1",
+        "publisher": "Doubleday",
+        "language": "English",
+        "pages": 454
+      },
+      {
+        "title": "The Girl with the Dragon Tattoo",
+        "author": "Stieg Larsson",
+        "publishedYear": 2005,
+        "genre": "Mystery",
+        "isbn": "978-0-307-59212-2",
+        "publisher": "Norstedts Förlag",
+        "language": "Swedish",
+        "pages": 672
+      },
+      {
+        "title": "The Hunger Games",
+        "author": "Suzanne Collins",
+        "publishedYear": 2008,
+        "genre": "Science Fiction",
+        "isbn": "978-0-439-02348-1",
+        "publisher": "Scholastic",
+        "language": "English",
+        "pages": 374
+      },
+      {
+        "title": "Catching Fire",
+        "author": "Suzanne Collins",
+        "publishedYear": 2009,
+        "genre": "Science Fiction",
+        "isbn": "978-0-439-02349-8",
+        "publisher": "Scholastic",
+        "language": "English",
+        "pages": 391
+      },
+      {
+        "title": "Mockingjay",
+        "author": "Suzanne Collins",
+        "publishedYear": 2010,
+        "genre": "Science Fiction",
+        "isbn": "978-0-439-02350-4",
+        "publisher": "Scholastic",
+        "language": "English",
+        "pages": 390
+      },
+      {
+        "title": "The Shining",
+        "author": "Stephen King",
+        "publishedYear": 1977,
+        "genre": "Horror",
+        "isbn": "978-0-385-12167-5",
+        "publisher": "Doubleday",
+        "language": "English",
+        "pages": 447
+      },
+      {
+        "title": "It",
+        "author": "Stephen King",
+        "publishedYear": 1986,
+        "genre": "Horror",
+        "isbn": "978-0-670-81302-4",
+        "publisher": "Viking",
+        "language": "English",
+        "pages": 1138
+      },
+      {
+        "title": "The Stand",
+        "author": "Stephen King",
+        "publishedYear": 1978,
+        "genre": "Horror",
+        "isbn": "978-0-385-12168-2",
+        "publisher": "Doubleday",
+        "language": "English",
+        "pages": 1153
+      },
+      {
+        "title": "The Hitchhiker's Guide to the Galaxy",
+        "author": "Douglas Adams",
+        "publishedYear": 1979,
+        "genre": "Science Fiction",
+        "isbn": "978-0-330-25864-7",
+        "publisher": "Pan Books",
+        "language": "English",
+        "pages": 193
+      },
+      {
+        "title": "Dune",
+        "author": "Frank Herbert",
+        "publishedYear": 1965,
+        "genre": "Science Fiction",
+        "isbn": "978-0-441-17271-7",
+        "publisher": "Chilton Books",
+        "language": "English",
+        "pages": 412
+      },
+      {
+        "title": "Neuromancer",
+        "author": "William Gibson",
+        "publishedYear": 1984,
+        "genre": "Science Fiction",
+        "isbn": "978-0-441-56956-3",
+        "publisher": "Ace Books",
+        "language": "English",
+        "pages": 271
+      },
+      {
+        "title": "Brave New World",
+        "author": "Aldous Huxley",
+        "publishedYear": 1932,
+        "genre": "Dystopian Fiction",
+        "isbn": "978-0-06-085052-4",
+        "publisher": "Chatto & Windus",
+        "language": "English",
+        "pages": 311
+      },
+      {
+        "title": "Fahrenheit 451",
+        "author": "Ray Bradbury",
+        "publishedYear": 1953,
+        "genre": "Dystopian Fiction",
+        "isbn": "978-0-7432-4722-1",
+        "publisher": "Ballantine Books",
+        "language": "English",
+        "pages": 158
+      },
+      {
+        "title": "The Road",
+        "author": "Cormac McCarthy",
+        "publishedYear": 2006,
+        "genre": "Post-apocalyptic Fiction",
+        "isbn": "978-0-307-38789-9",
+        "publisher": "Knopf",
+        "language": "English",
+        "pages": 287
+      },
+      {
+        "title": "The Martian",
+        "author": "Andy Weir",
+        "publishedYear": 2011,
+        "genre": "Science Fiction",
+        "isbn": "978-0-553-41802-6",
+        "publisher": "Crown",
+        "language": "English",
+        "pages": 387
+      },
+      {
+        "title": "Jurassic Park",
+        "author": "Michael Crichton",
+        "publishedYear": 1990,
+        "genre": "Science Fiction",
+        "isbn": "978-0-345-37077-8",
+        "publisher": "Knopf",
+        "language": "English",
+        "pages": 399
+      },
+      {
+        "title": "The Girl on the Train",
+        "author": "Paula Hawkins",
+        "publishedYear": 2015,
+        "genre": "Mystery",
+        "isbn": "978-1-59463-366-9",
+        "publisher": "Riverhead Books",
+        "language": "English",
+        "pages": 336
+      },
+      {
+        "title": "Gone Girl",
+        "author": "Gillian Flynn",
+        "publishedYear": 2012,
+        "genre": "Mystery",
+        "isbn": "978-0-307-58836-1",
+        "publisher": "Crown Publishing Group",
+        "language": "English",
+        "pages": 419
+      },
+      {
+        "title": "The Silent Patient",
+        "author": "Alex Michaelides",
+        "publishedYear": 2019,
+        "genre": "Thriller",
+        "isbn": "978-1-250-30169-7",
+        "publisher": "Celadon Books",
+        "language": "English",
+        "pages": 336
+      },
+      {
+        "title": "Educated",
+        "author": "Tara Westover",
+        "publishedYear": 2018,
+        "genre": "Memoir",
+        "isbn": "978-0-399-59050-4",
+        "publisher": "Random House",
+        "language": "English",
+        "pages": 334
+      },
+      {
+        "title": "Becoming",
+        "author": "Michelle Obama",
+        "publishedYear": 2018,
+        "genre": "Memoir",
+        "isbn": "978-1-5247-6313-8",
+        "publisher": "Crown Publishing Group",
+        "language": "English",
+        "pages": 426
+      },
+    ]
+
+   // const bookUser = books.filter((bk) => bk.publishedYear > 2012 )
+
+   const bookUser = books.filter((bk) => { return bk.genre == "Fiction" && bk.pages >=200})
+    console.log(bookUser)
