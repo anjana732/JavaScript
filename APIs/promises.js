5ee71b9bc6ccc41902aaf77a2159e493cbbf7b37
anjana732 <anjanasingh1257@gmail.com>
2024-02-06 23:58:08 +0530

    completed promises

--- APIs/promises.js
+++ APIs/promises.js
@@ -1,5 +1,72 @@
 const promise1 = new Promise(function(resolve,reject){
     setTimeout(() => {
         console.log("inside promise 1");
+        resolve();
     },1000)
 })
+//consuming promise1
+
+const consumePromise = promise1.then(()=>{
+    console.log("inside consume promise 1");
+})
+
+//creation of promise 2 which includes parameter in resolve
+
+const promise2 = new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        console.log("inside promise 2");
+        resolve({username:"Anjana",password:"123"})
+    },1000)
+})
+
+//consuming promise 2
+
+const promise2Consumption = promise2.then((response)=>{
+    console.log(response);
+})
+
+//other way of promise consumption
+
+const promise3 = new Promise((resolve,reject)=>{
+    console.log("inside promise 3");
+    resolve({username:"singh", password:"123"});
+}).then((response)=>{
+    console.log(response);
+})
+
+const promise4 = new Promise((resolve,reject)=>{
+    setTimeout(function(){
+        console.log("inside promise 4")
+        let error = true;
+        if(!error){
+            resolve({username:"singh", password:"123"})
+        }else{
+            reject("something went wrong")
+        }
+    },1000)
+}).then((response)=>{
+    console.log(response)
+}).catch((err)=>{
+    console.log(err)
+}).finally("either one of the above two has been executed")
+
+//use of async and await
+
+const promise5 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("Inside promise 5");
+        let error = false;
+        if(!error){
+            resolve({username:"Anjana singh", password:"123"})
+        }else{
+            reject("someting went wrong")
+        }
+    },1000)
+})
+
+async function learnPromise(){
+    const data = await promise5
+    console.log(data)
+}
+
+learnPromise()
